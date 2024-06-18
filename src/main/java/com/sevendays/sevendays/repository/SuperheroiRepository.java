7a5eeb752c034433b042205fd4b34ac00a44fb41
Maria Isabel <mipt1612@gmail.com>
2024-06-18 14:16:48 -0300

    Adicionar desafio do quinto dia

--- src/main/java/com/sevendays/sevendays/repository/SuperheroiRepository.java
+++ src/main/java/com/sevendays/sevendays/repository/SuperheroiRepository.java
@@ -1,9 +1,19 @@
 package com.sevendays.sevendays.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sevendays.sevendays.model.Superheroi;
 
-public interface SuperheroiRepository extends JpaRepository<Superheroi, Long>{
+import io.swagger.v3.oas.annotations.Hidden;
 
+@Hidden
+public interface SuperheroiRepository extends JpaRepository<Superheroi, Long>{
+    @Query("SELECT s FROM Superheroi s WHERE LOWER(s.poderes) LIKE LOWER(CONCAT('%', :poderes, '%'))")
+    List<Superheroi> findByPoderesContaining(@Param("poderes") String poderes);
+
+    List<Superheroi> findByPoderesContainingIgnoreCase(String poderes);
 }
