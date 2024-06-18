a4e1e72e0f063f1a2cbb5e01da0890a2e1c2bca4
Maria Isabel <mipt1612@gmail.com>
2024-06-18 13:47:04 -0300

    Adicionar desafio do segundo dia

---
+++ src/main/java/com/sevendays/sevendays/repository/SuperheroiRepository.java
@@ -0,0 +1,9 @@
+package com.sevendays.sevendays.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sevendays.sevendays.model.Superheroi;
+
+public interface SuperheroiRepository extends JpaRepository<Superheroi, Long>{
+
+}
