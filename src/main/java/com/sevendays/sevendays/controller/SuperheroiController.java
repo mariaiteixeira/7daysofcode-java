4181d9bcf25427e761d9154f19d332c2df0199ec
Maria Isabel <mipt1612@gmail.com>
2024-06-18 13:55:53 -0300

    Adicionar desafio do terceiro dia

---
+++ src/main/java/com/sevendays/sevendays/controller/SuperheroiController.java
@@ -0,0 +1,43 @@
+package com.sevendays.sevendays.controller;
+
+import com.sevendays.sevendays.repository.SuperheroiRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.sevendays.sevendays.model.Superheroi;
+
+@RestController
+@RequestMapping("/superherois")
+public class SuperheroiController {
+
+    @Autowired    
+    private SuperheroiRepository repository;
+
+    @GetMapping
+    public List<Superheroi> listar() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public List<Superheroi> adicionarLista(@RequestBody List<Superheroi> superherois) {
+        return repository.saveAll(superherois);
+    }
+    
+    @PutMapping
+    public Superheroi alterar(@RequestBody Superheroi superheroi) {
+        if (superheroi.getId() > 0) {
+            return repository.save(superheroi);
+        }
+        return null;
+    }
+
+    @DeleteMapping
+    public String deletar(@RequestBody Superheroi superheroi) {
+        if (superheroi.getId() > 0) {
+            repository.delete(superheroi);
+            return "Removido com sucesso";
+        }
+        return "Superherói não encontrado";
+    }
+}
